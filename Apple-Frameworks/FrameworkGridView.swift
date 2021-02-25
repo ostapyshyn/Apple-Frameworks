0d711bdcfca970988b8a8d8b93d62da0f0fad505
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2021-02-25 22:49:44 +0200

    Add  model

--- Apple-Frameworks/FrameworkGridView.swift
+++ Apple-Frameworks/FrameworkGridView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct FrameworkGridView: View {
     
+    @StateObject var viewModel = FrameworkGridViewModel()
+    
     let columns: [GridItem] = [GridItem(.flexible()),
                                GridItem(.flexible()),
                                GridItem(.flexible())]
@@ -19,6 +21,9 @@ struct FrameworkGridView: View {
                 LazyVGrid(columns: columns) {
                     ForEach(MockData.frameworks) { framework in
                         FrameworkTitleView(framework: framework)
+                            .onTapGesture {
+                                viewModel.selectedFramework = framework
+                            }
                     }
                 }
             }
@@ -30,7 +35,8 @@ struct FrameworkGridView: View {
 struct FrameworkGridView_Previews: PreviewProvider {
     static var previews: some View {
         FrameworkGridView()
-    }
+            .preferredColorScheme(.dark)
+    }c
 }
 
 struct FrameworkTitleView: View {
---
+++ Apple-Frameworks/FrameworkGridViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  FrameworkGridViewModel.swift
+//  Apple-Frameworks
+//
+//  Created by Volodymyr Ostapyshyn on 25.02.2021.
+//
+
+import SwiftUI
+
+final class FrameworkGridViewModel: ObservableObject {
+    
+    var selectedFramework: Framework?
+    
+}
